c1bc5aaafbb7ce181049ad2b6b9513f8002c6454
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-22 13:57:00 +0600

    load data from server

--- js/api.js
+++ js/api.js
@@ -3,3 +3,24 @@ function loadData(){
   .then(response => response.json())
   .then(data => console.log(data))
 }
+
+function getUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => displayUser(data))
+}
+  function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(resp =>resp.json())
+      .then(data =>console.log(data))
+  }
+
+  function displayUser(users){
+    const ul = document.getElementById('users')
+    for(const user of users ){
+        console.log(user)
+        const li = document.createElement('li')
+        li.innerText=`name: ${user.name},ID: ${user.id}, Email: ${user.email}`
+        ul.appendChild(li)
+    }
+  }
---
+++ js/pactis.js
@@ -0,0 +1,6 @@
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    
+}
