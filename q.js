28323c499d699a2a72739a7366c5dd48d8b3a650
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-22 20:35:43 +0600

    add all

---
+++ q.js
@@ -0,0 +1,9 @@
+const stuInfo = JSON.stringify({ name: "James", roll: 3 }); 
+/* console.log(stuInfo.name);  */
+//q-3
+const premikas= { name: 'keka ferdousi' , cars: { brand: 'toyota'} }
+const{brand}=premikas.cars
+console.log(brand);
+// Q-15
+const info={ data: [ { language:"Javascript", library:"React" } ] }  
+console.log(info.data[0].library)
